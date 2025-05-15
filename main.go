d66f896f5a37b6ba00d3a2e159cb8da35a5f2b7b
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-14 22:43:15 -0600

    creado comentario para uso posterior

--- main.go
+++ main.go
@@ -50,6 +50,13 @@ func main() {
 	} */
 
 	e.GET("/", func(c echo.Context) error {
+		/* ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		rows, err := dbConnection.QueryContext(ctx, "select * from mascotas")
+		if err != nil {
+			return c.HTML(http.StatusInternalServerError, "<h1>Internal server error</h1>")
+		} */
+
 		response := map[string]any{
 			"URL":          URL,
 			"CurrentRoute": "/",
